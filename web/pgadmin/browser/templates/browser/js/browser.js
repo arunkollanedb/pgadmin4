0ab5ab40a53f6073e2fed1be63b7ff5e245acd17
Ashesh Vashi <ashesh.vashi@enterprisedb.com>
2015-06-09 03:14:13 +0530

    Added comments in the pgAdmin.Browser file

--- web/pgadmin/browser/templates/browser/js/browser.js
+++ web/pgadmin/browser/templates/browser/js/browser.js
@@ -6,8 +6,10 @@ define(
         'jquery.acitree', 'codemirror.sql', 'pgadmin.alertifyjs'],
 function(require, $, _, Bootstrap, pgAdmin, alertify, CodeMirror) {
 
+    // Some scripts do export their object in the window only.
+    // Generally the one, which do no have AMD support.
     var wcDocker = window.wcDocker;
-    $ = $ || window.jQuery;
+    $ = $ || window.jQuery || window.$;
     Bootstrap = Bootstrap || window.Bootstrap;
 
     pgAdmin.Browser = pgAdmin.Browser || {};
@@ -37,14 +39,25 @@ WITH ( \n\
 ALTER TABLE tickets_detail \n\
 OWNER TO helpdesk;\n';
 
+    // Extend the browser class attributes
     _.extend(pgAdmin.Browser, {
+        // The base url for browser
         URL: {{ url_for('browser.index') }},
+        // We do have docker of type wcDocker to take care of different
+        // containers. (i.e. panels, tabs, frames, etc.)
         docker:null,
+        // Reversed Engineer query for the selected database node object goes
+        // here
         editor:null,
+        // Left hand browser tree
         tree:null,
-        /* list of script to be loaded, when a certain type of node is loaded */
+        // list of script to be loaded, when a certain type of node is loaded
+        // It will be used to register extensions, tools, child node scripts,
+        // etc.
         scripts: {},
+        // Default panels
         panels: {
+            // Panel to keep the left hand browser tree
             'browser': new pgAdmin.Browser.Panel({
                 name: 'browser',
                 title: '{{ _('Browser') }}',
@@ -53,6 +66,7 @@ OWNER TO helpdesk;\n';
                 isPrivate: true,
                 content: '<div id="tree" class="aciTree"></div>'
             }),
+            // Properties of the object node
             'properties': new pgAdmin.Browser.Panel({
                 name: 'properties',
                 title: '{{ _('Properties') }}',
@@ -61,6 +75,7 @@ OWNER TO helpdesk;\n';
                 isPrivate: true,
                 content: '<div id="obj_props" class="obj_properties">No object selected!</div>'
             }),
+            // Statistics of the object
             'statistics': new pgAdmin.Browser.Panel({
                 name: 'statistics',
                 title: '{{ _('Statistics') }}',
@@ -69,6 +84,7 @@ OWNER TO helpdesk;\n';
                     isPrivate: true,
                     content: '<p>Statistics pane</p>'
                 }),
+            // Reversed engineered SQL for the object
             'sql': new pgAdmin.Browser.Panel({
                 name: 'sql',
                 title: '{{ _('SQL') }}',
@@ -78,6 +94,7 @@ OWNER TO helpdesk;\n';
                 // TODO:: Revove demoSql later
                 content: '<textarea id="sql-textarea" name="sql-textarea">' + demoSql + '</textarea>'
             }),
+            // Dependencies of the object
             'dependencies': new pgAdmin.Browser.Panel({
                 name: 'dependencies',
                 title: '{{ _('Dependencies') }}',
@@ -86,6 +103,7 @@ OWNER TO helpdesk;\n';
                 isPrivate: true,
                 content: '<p>Depedencies pane</p>'
             }),
+            // Dependents of the object
             'dependents': new pgAdmin.Browser.Panel({
                 name: 'dependents',
                 title: '{{ _('Dependents') }}',
@@ -93,7 +111,7 @@ OWNER TO helpdesk;\n';
                 isClosable: false,
                 isPrivate: true,
                 content: '<p>Dependent pane</p>'
-            })/* Add hooked-in panels */{% for panel_item in current_app.panels %}{% if not panel_item.isIframe %},'{{ panel_item.name }}' : new pgAdmin.Browser.Panel({
+            })/* Add hooked-in panels by extensions */{% for panel_item in current_app.panels %}{% if not panel_item.isIframe %},'{{ panel_item.name }}' : new pgAdmin.Browser.Panel({
                 name: '{{ panel_item.name }}',
                 title: '{{ panel_item.title }}',
                 width: {{ panel_item.width }},
@@ -104,7 +122,9 @@ OWNER TO helpdesk;\n';
                 content: '{{ panel_item.content }}'
             }){% endif %}{% endfor %}
         },
+        // We also support showing dashboards, HTML file, external URL
         frames: {
+            // Dashboard
             'dashboard': new pgAdmin.Browser.Frame({
                 name: 'dashboard',
                 title: '{{ _('Dashboard') }}',
@@ -112,7 +132,7 @@ OWNER TO helpdesk;\n';
                 isClosable: false,
                 isPrivate: true,
                 url: 'about:blank' /* TODO:: Change it with http://www.pgadmin.org later */
-            })/* Add hooked-in frames */{% for panel_item in current_app.panels %}{% if panel_item.isIframe %},
+            })/* Add hooked-in frames by extensions */{% for panel_item in current_app.panels %}{% if panel_item.isIframe %},
             '{{ panel_item.name }}' : new pgAdmin.Browser.Frame({
                 name: '{{ panel_item.name }}',
                 title: '{{ panel_item.title }}',
@@ -124,17 +144,27 @@ OWNER TO helpdesk;\n';
                 url: '{{ panel_item.content }}'
             }){% endif %}{% endfor %}
         },
-        modules: [],
         /* Menus */
+        // pgAdmin.Browser.MenuItem.add_menus(...) will register all the menus
+        // in this container
         menus: {
+            // All context menu goes here under certain menu types.
+            // i.e. context: {'server': [...], 'server-group': [...]}
             context: {},
+            // File menus
             file: {},
+            // Edit menus
             edit: {},
+            // Object menus
             object: {},
+            // Management menus
             management: {},
+            // Tools menus
             tools: {},
+            // Help menus
             help: {}
         },
+        // A callback to load/fetch a script when a certain node is loaded
         register_script: function(n, m, p) {
             var scripts = this.scripts;
             scripts[n] = _.isArray(scripts[n]) ? scripts[n] : [];
@@ -142,26 +172,44 @@ OWNER TO helpdesk;\n';
         },
         // Build the default layout
         buildDefaultLayout: function() {
-            this.docker.addPanel('dashboard', wcDocker.DOCK_TOP, this.panels['properties'].panel);
-            this.docker.addPanel('properties', wcDocker.DOCK_STACKED, this.frames['dashboard'].panel);
-            this.docker.addPanel('sql', wcDocker.DOCK_STACKED, this.frames['dashboard'].panel);
-            this.docker.addPanel('statistics', wcDocker.DOCK_STACKED, this.frames['dashboard'].panel);
-            this.docker.addPanel('dependencies', wcDocker.DOCK_STACKED, this.frames['dashboard'].panel);
-            this.docker.addPanel('dependents', wcDocker.DOCK_STACKED, this.frames['dashboard'].panel);
+            this.docker.addPanel('dashboard', wcDocker.DOCK_TOP,
+                    this.panels['properties'].panel);
+            this.docker.addPanel('properties', wcDocker.DOCK_STACKED,
+                    this.frames['dashboard'].panel);
+            this.docker.addPanel('sql', wcDocker.DOCK_STACKED,
+                    this.frames['dashboard'].panel);
+            this.docker.addPanel('statistics', wcDocker.DOCK_STACKED,
+                    this.frames['dashboard'].panel);
+            this.docker.addPanel('dependencies', wcDocker.DOCK_STACKED,
+                    this.frames['dashboard'].panel);
+            this.docker.addPanel('dependents', wcDocker.DOCK_STACKED,
+                    this.frames['dashboard'].panel);
 
-            this.docker.addPanel('browser', wcDocker.DOCK_LEFT, this.frames['dashboard'].panel);
+            this.docker.addPanel('browser', wcDocker.DOCK_LEFT,
+                    this.frames['dashboard'].panel);
         },
         // Enable/disable menu options
         enable_disable_menus: function(item) {
-            /* New object menu mechanism */
+            // Mechanism to enable/disable menus depending on the condition.
             var obj = this, j, e,
+                // menu navigation bar
                 navbar = $('#navbar-menu > ul').first(),
+                // Drop down menu for objects
                 obj_mnu = navbar.find('li#mnu_obj > ul#mnu_dropdown_obj').first(),
+                // Drop down menu for create object
                 create_mnu = navbar.find("#mnu_create_obj").empty(),
+                // data for current selected object
                 d = this.tree.itemData(item);
+
+            // All menus from the object menus (except the create drop-down
+            // menu) needs to be removed.
             obj_mnu.children("li:not(:first-child)").remove();
+            // Create a dummy 'no object seleted' menu
             create_mnu.html('<li class="menu-item disabled"><a href="#">{{ _('No object selected') }}</a></li>\n');
 
+            // All menus (except for the object menus) are already present.
+            // They will just require to check, wheather they are
+            // enabled/disabled.
             _.each([
                     {m: 'file', id: '#mnu_file'},
                     {m: 'edit', id: '#mnu_edit'},
@@ -171,15 +219,20 @@ OWNER TO helpdesk;\n';
                         j = navbar.children(o.id).children('.dropdown-menu').first();
                         _.each(obj.menus[o.m],
                             function(v, k) {
+                                // Remove disabled class in any case first.
                                 e = j.find('#' + k).closest('.menu-item').removeClass('disabled');
                                 if (v.disabled(obj)) {
+                                    // Make this menu disabled
                                     e.addClass('disabled');
                                 }
                             });
                     });
 
+            // Create the object menu dynamically
             if (item && this.menus['object'] && this.menus['object'][d._type]) {
                 var create_items = [];
+                // The menus will be created based on the priority given.
+                // Menu with lowest value has the highest priority.
                 _.each(_.sortBy(
                             this.menus['object'][d._type],
                             function(o) { return o.priority; }),
@@ -190,7 +243,7 @@ OWNER TO helpdesk;\n';
                                 obj_mnu.append(m.generate());
                             }
                         });
-                /* Create menus goes seperately */
+                // Create menus goes seperately
                 if (create_items.length > 0) {
                     create_mnu.empty();
                     _.each(create_items, function(c) {
@@ -198,14 +251,7 @@ OWNER TO helpdesk;\n';
                     });
                 }
             }
-        },
-        // Get the selected treeview item type, or nowt
-        get_selected_node_type: function() {
-            item = this.tree.selected();
-            if (!item || item.length != 1)
-                return "";
 
-            return this.tree.itemData(item)._type;
         },
         init: function() {
             var obj=this;
@@ -220,15 +266,19 @@ OWNER TO helpdesk;\n';
                 return true;
             });
 
+            // Initialize the Docker
             obj.docker = new wcDocker('#dockerContainer');
             if (obj.docker) {
+                // Initialize all the panels
                 _.each(obj.panels, function(panel, name) {
                     obj.panels[name].load(obj.docker);
                 });
+                // Initialize all the frames
                 _.each(obj.frames, function(frame, name) {
                     obj.frames[name].load(obj.docker);
                 });
 
+                // Stored layout in database from the previous session
                 var layout = '{{ layout }}';
 
                 // Try to restore the layout if there is one
@@ -343,7 +393,9 @@ OWNER TO helpdesk;\n';
                             }]);
                     }
                 }
-                switch (eventName){
+                switch (eventName) {
+                    // When a node is added in the browser tree, we need to
+                    // load the registered scripts
                     case "added":
                         if (d) {
                             /* Loading all the scripts registered to be loaded on this node */
@@ -352,6 +404,7 @@ OWNER TO helpdesk;\n';
                                     if (!s.loaded) {
                                         require([s.name], function(m) {
                                             s.loaded = true;
+                                            // Call the initialize (if present)
                                             if (m && m.init && typeof m.init == 'function') {
                                                 try {
                                                     m.init();
@@ -367,47 +420,60 @@ OWNER TO helpdesk;\n';
                         break;
                 }
             });
+
+            // There are some scripts which needed to be loaded immediately,
+            // but - not all. We will will need to generate all the menus only
+            // after they all were loaded completely.
             var counter = {total: 0, loaded: 0};
-            {% for script in current_app.javascripts %}{% if 'when' in script %}counter.total += 1; /* Not loading it now */
-            {% if script.when %}counter.loaded += 1; /* Not loading it now */
-            /* Registering '{{ script.path }}.js' to be loaded when a node '{{ script.when }}' is loaded */
-            this.register_script('{{ script.when }}', '{{ script.name }}', '{{ script.path }}.js');{% else %}
-            /* Loading {{ script.path }} */
+            {% for script in current_app.javascripts %}{% if 'when' in script %}
+            {% if script.when %}/* Registering '{{ script.path }}.js' to be loaded when a node '{{ script.when }}' is loaded */
+            this.register_script('{{ script.when }}', '{{ script.name }}', '{{ script.path }}.js');{% else %}/* Loading '{{ script.path }}' */
+            counter.total += 1;
             this.load_module('{{ script.name }}', '{{ script.path }}', counter);{% endif %}{% endif %}{% endfor %}
 
             var geneate_menus = function() {
-                    if (counter.total == counter.loaded) {
-                        {% set cnt = 1 %}
-                        obj.add_menus([{% for key in ('File', 'Edit', 'Object' 'Tools', 'Management', 'Help') %}{% for item in current_app.menu_items['%s_items' % key.lower()] %}{% if cnt != 1 %}, {% endif %} {
-                            name: "{{ item.name }}",
-                            {% if item.module %}module: {{ item.module }},
-                            {% endif %}{% if item.url %}url: "{{ item.url }}",
-                            {% endif %}{% if item.target %}target: "{{ item.target }}",
-                            {% endif %}{% if item.callback %}callback: "{{ item.callback }}",
-                            {% endif %}label: '{{ item.label }}', applies: ['{{ key.lower() }}'],
-                            priority: {{ item.priority }},
-                            enable: '{{ item.enable }}'
-                        }{% set cnt = cnt + 1 %}{% endfor %}{% set cnt = cnt + 1 %}{% endfor %}]);
-                        obj.create_menus();
-                    } else {
-                        setTimeout(function() { geneate_menus(); }, 500);
-                    }
+                // Generate the menu items only when all the initial scripts
+                // were loaded completely.
+                //
+                // First - register the menus from the other
+                // modules/extensions.
+                if (counter.total == counter.loaded) {
+                    {% set cnt = 1 %}
+                    obj.add_menus([{% for key in ('File', 'Edit', 'Object' 'Tools', 'Management', 'Help') %}{% for item in current_app.menu_items['%s_items' % key.lower()] %}{% if cnt != 1 %}, {% endif %} {
+                        name: "{{ item.name }}",
+                        {% if item.module %}module: {{ item.module }},
+                        {% endif %}{% if item.url %}url: "{{ item.url }}",
+                        {% endif %}{% if item.target %}target: "{{ item.target }}",
+                        {% endif %}{% if item.callback %}callback: "{{ item.callback }}",
+                        {% endif %}label: '{{ item.label }}', applies: ['{{ key.lower() }}'],
+                        priority: {{ item.priority }},
+                        enable: '{{ item.enable }}'
+                    }{% set cnt = cnt + 1 %}{% endfor %}{% set cnt = cnt + 1 %}{% endfor %}]);
+                    obj.create_menus();
+                } else {
+                    // recall after some time
+                    setTimeout(function() { geneate_menus(); }, 300);
+                }
             };
             geneate_menus();
         },
+        // load the module right now
         load_module: function(name, path, c) {
             require([name],function(m) {
                 try {
+                    // initialze the module (if 'init' function present).
                     if (m.init && typeof(m.init) == 'function')
                         m.init();
                 } catch (e) {
                 }
-                c.loaded += 1;
+                if (c)
+                    c.loaded += 1;
             }, function() {
-                /* TODO:: proper error */
+                /* TODO:: Show proper error */
                 console.log (arguments);
             });
         },
+        // Add menus of module/extension at appropriate menu
         add_menus: function(menus) {
             var pgMenu = this.menus;
             var MenuItem = pgAdmin.Browser.MenuItem;
@@ -449,6 +515,7 @@ OWNER TO helpdesk;\n';
                 });
             });
         },
+        // Create the menus
         create_menus: function() {
             /* Create menus */
             var navbar = $('#navbar-menu > ul').first();
